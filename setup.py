c05da28d8f9235db3670c9896f983c08650e9fec
H-goto16 <h.goto.engineer@gmail.com>
2023-08-16 21:41:11 +0900

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r", encoding="utf-8") as fh:
+    license = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    requirements = req_file.read().splitlines()
+
+
+
+setup(
+    name='online_audio_kit',
+    version=VERSION,
+    author='Haruki Goto',
+    author_email='h.goto.engineer@gmail.com',
+    description='simple audio kit.',  
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=license,
+    url='https://github.com/rionehome/online_audio_kit',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
