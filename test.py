a3159cbed08aa88f13f293cd67e95153bf4069d6
H-goto16 <h.goto.engineer@gmail.com>
2024-04-19 20:34:17 +0900

    fix: delete files

--- test.py
+++ test.py
@@ -5,8 +5,9 @@ client = imageWithGPT(
   # api_key=""
 )
 res = client.get(
-  image=Image.open("./man.jpg"),
-  prompt="please tell me humans clothes color."
+  image=Image.open("./hamada.jpg"),
+  prompt="Please extract this data from image. [height,age,gender,hair color,color of pants,Whether or not to wear a mask,Whether or not to wear glasses,Whether or not to wear a hat,color of pants,color of clothes,sleeve length] and please return array"
+
 )
 
 print(res)
